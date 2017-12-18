57d6944014f1c2deb22965bda166d794004d71e8
andy <andy@192.168.8.152>
2017-12-18 16:37:49 +0800

    Filter模块测试通过

--- src/Phalcon/Filter.php
+++ src/Phalcon/Filter.php
@@ -23,14 +23,14 @@ use \Phalcon\Filter\Exception as FilterException;
  * object oriented wrappers to the php filter extension. Also allows the developer to
  * define his/her own filters
  *
- *<code>
+ * <code>
  * $filter = new \Phalcon\Filter();
  *
  * $filter->sanitize("some(one)@exa\\mple.com", "email"); // returns "someone@example.com"
  * $filter->sanitize("hello<<", "string"); // returns "hello"
  * $filter->sanitize("!100a019", "int"); // returns "100019"
  * $filter->sanitize("!100a019.01a", "float"); // returns "100019.01"
- *</code>
+ * </code>
  */
 class Filter implements FilterInterface
 {
@@ -121,6 +121,7 @@ class Filter implements FilterInterface
                 //@note no type check of $key
                 $sanizitedValue[$key] = $this->_sanitize($itemValue, $filters);
             }
+            return $sanizitedValue;
         }
 
         return $this->_sanitize($value, $filters);
